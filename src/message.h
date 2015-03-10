d67a1a26fbeff993e652a0a6cc31f1d95d0d5926
Stephan Linz <linz@li-pro.net>
2015-03-10 17:32:11 +0100

    [RPM] doxygen-1.8.6-older_flex.patch
    With release 1.8.6 a newer version of flex is required to build doxygen.
    Try version 2.5.35 or later.
    
    See also: https://bugzilla.gnome.org/show_bug.cgi?id=719525
    
    From: Suzumizaki-Kimitaka
    Date: 2013-11-29 01:50 UTC
    Subject: Define ***YY_flex_debug to finish build doxygen
    
    After SHA-1: 3c0a246bd1bcf6f0d560394fd81616260a174ae8 (2013-11-14),
    I cannot build doxygen because some definitions don't exist.
    
    All ***YY_flex_debug integers seem to be NOT defined anywhere in the
    source tree. Or do I mistake something to build?
    
    I made the patch to roughly resolve this problem.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- src/message.h
+++ src/message.h
@@ -31,5 +31,32 @@ extern void warn_uncond(const char *fmt, ...);
 extern void err(const char *fmt, ...);
 void initWarningFormat();
 
+/*
+After SHA-1: 3c0a246bd1bcf6f0d560394fd81616260a174ae8 (2013-11-14),
+we cannot build doxygen because the definitions below are not found.
+The constants below are used as the first parameter of printlex() function.
+*/
+enum
+{
+	fortranscannerYY_flex_debug = 1, // Should not be zero. see the definition of printlex() on message.cpp.
+	preYY_flex_debug,
+	fortrancodeYY_flex_debug,
+	defargsYY_flex_debug,
+	doctokenizerYY_flex_debug,
+	pycodeYY_flex_debug,
+	vhdlcodeYY_flex_debug,
+	vhdlscannerYY_flex_debug,
+	pyscannerYY_flex_debug,
+	scannerYY_flex_debug,
+	declinfoYY_flex_debug,
+	commentscanYY_flex_debug,
+	configYY_flex_debug,
+	commentcnvYY_flex_debug,
+	cppExpYY_flex_debug,
+	codeYY_flex_debug,
+	constexpYY_flex_debug,
+	tclscannerYY_flex_debug,
+};
+
 extern void printlex(int dbg, bool enter, const char *lexName, const char *fileName);
 #endif
